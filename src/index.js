a97605c007542c269468b6febc112e7bc0be3b49
Alexey Filatov <alexeyfilatov994@gmail.com>
2023-02-24 22:04:53 +0300

    feat: html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import './main.scss';
+import './styles/main.scss';
 import './fonts/fonts.scss';
+require('./icon_customImage.js');
 
+// header
+const header = document.querySelector('header');
+const navToggle = header.querySelector('.nav-toggle');
+const linksToggle = header.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+  header.classList.toggle('active');
+  navToggle.classList.toggle('active');
+  linksToggle.classList.toggle('active');
+});
+
+// modal
+const modalButton = document.querySelector('.modal-button');
+const modalOverlay = document.querySelector('.modal-overlay');
+const modalClose = document.querySelector('.modal-close');
+
+modalButton.addEventListener('click', () => {
+  modalOverlay.classList.add('show');
+});
+
+modalClose.addEventListener('click', () => {
+  modalOverlay.classList.remove('show');
+});
