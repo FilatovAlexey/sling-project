542e84d48e1f7e1e25cefe8d73094d764ec9ef90
Alexey Filatov <alexeyfilatov994@gmail.com>
2023-02-26 16:16:09 +0300

    feat: privacy

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,78 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, "src", "index.js"),
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'index.[contenthash].js',
-    assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
+    path: path.join(__dirname, "dist"),
+    filename: "index.[contenthash].js",
+    assetModuleFilename: path.join("images", "[name].[contenthash][ext]"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html'),
-      filename: 'index.html',
+      template: path.join(__dirname, "src", "index.html"),
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "privacy.html"),
+      filename: "privacy.html",
     }),
     new FileManagerPlugin({
       events: {
         onStart: {
-          delete: ['dist'],
+          delete: ["dist"],
         },
         onEnd: {
           copy: [
             {
-              source: path.join('src', 'static'),
-              destination: 'dist',
+              source: path.join("src", "static"),
+              destination: "dist",
             },
           ],
         },
       },
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
+      filename: "[name].[contenthash].css",
     }),
   ],
   devServer: {
-    watchFiles: path.join(__dirname, 'src'),
+    watchFiles: path.join(__dirname, "src"),
     port: 9000,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.(scss|css)$/,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
         ],
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.svg$/,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: path.join('icons', '[name].[contenthash][ext]'),
+          filename: path.join("icons", "[name].[contenthash][ext]"),
         },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
@@ -79,10 +83,10 @@ module.exports = {
           implementation: ImageMinimizerPlugin.imageminMinify,
           options: {
             plugins: [
-              ['gifsicle', { interlaced: true }],
-              ['jpegtran', { progressive: true }],
-              ['optipng', { optimizationLevel: 5 }],
-              ['svgo', { name: 'preset-default' }],
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              ["svgo", { name: "preset-default" }],
             ],
           },
         },
